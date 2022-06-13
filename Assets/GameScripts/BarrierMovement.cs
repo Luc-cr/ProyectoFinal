ff616eb5244b73f5db789e09a9fdb86167ccb812
Lucas <72467699+Luc-cr@users.noreply.github.com>
2022-06-13 14:26:41 -0300

    13/6/2022Collision

--- Assets/GameScripts/BarrierMovement.cs
+++ Assets/GameScripts/BarrierMovement.cs
@@ -1,15 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class BarrierMovement : MonoBehaviour {
 
     // Publics Vars
     public GameObject prefab;
     public int barrierSpeed;
+    public Text points;
+    public GameObject player;
     // Private Vars
     private GameObject obj;
-
+    private int score = 0;
 
     void Start () {
         // Crear una barrera
@@ -17,16 +20,21 @@ public class BarrierMovement : MonoBehaviour {
         obj = Instantiate(prefab);
         obj.transform.position = new Vector3(Random.Range(-2.00f, 7.00f), -6, 5);
     }
-	
-	void Update () {
+
+    void Update()
+    {
         // Movimiento de la barrera
         obj.transform.Translate(Vector3.forward * -barrierSpeed * Time.deltaTime);
-        
-        // 
-        if(obj.transform.position == new Vector3(2.00f,-6.00f,-14.00f))
+
+        if (obj.transform.position.z <= -13f)
         {
             Debug.Log("E: OBJ Destoy");
+            score++;
+            points.text = "Puntaje " + score.ToString();
             Destroy(obj);
+            barrierSpeed++;
+            obj = Instantiate(prefab);
+            obj.transform.position = new Vector3(Random.Range(-2.00f, 7.00f), -6, 5);
         }
     }
 }
---
+++ Assets/GameScripts/PlayerCollision.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour {
+
+    public GameObject player;
+	void Start () {
+		
+	}
+	
+    void OnCollisionEnter(Collision coll)
+    {
+        if(coll.gameObject.name == "Cube_2" || coll.gameObject.name == "Cube_1")
+        {
+            Destroy(player);
+            
+        }
+    }
+    
+}
